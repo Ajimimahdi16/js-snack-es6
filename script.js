867558da1012f4a87dfe4803a46760b4d6f7b656
AJIMI MAHDI <AJIMIMAHDI1@GMAIL.COM>
2026-01-10 17:56:46 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+/*Snack 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore.*/
+
+const bikes = {
+    nome: "Bike1",
+    peso: 10
+};
+
+const bikes2 = {
+    nome: "Bike2",
+    peso: 15 
+};
+
+const bikes3 = {
+    nome: "Bike3",
+    peso: 8
+};
+ const bikes4 ={
+    nome: "Bike4",
+    peso: 12
+ };
+ const bikes5 = {
+    nome: "Bike5",
+    peso: 11
+ };
+
+ 
+
+
+/*Snack 2
+Creare un array di oggetti di squadre.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà “punti fatti” e “falli subiti”.
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.*/
