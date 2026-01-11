db36bf0c4037d664100466b954e12662fa913b81
AJIMI MAHDI <AJIMIMAHDI1@GMAIL.COM>
2026-01-11 17:55:05 +0100

    snack2 inserimento come in connsegna delle squadre di calcio ho scelto di mettere 5 squadre con variabile nome e al interno gli oggetti nome puntiFatti falliSubiti  come in consegna ci ha chiesto di indendificare solo il nome della squadra invece il restante e 0 ho racchiuso tutti in una arrayteams con tutte le squadre inserite in precedenza aprendo un ciclo for e inserendo i punti e falli subiti in numeri random apposto dei 0 fino a 100 facendo arrayteams di index .puntifatti/.fallisubiti = math.floor (math.random() per 100

--- script.js
+++ script.js
@@ -41,8 +41,43 @@ const bikes3 = {
 
 
 /*Snack 2
-Creare un array di oggetti di squadre.
+
+
+/*Creare un array di oggetti di squadre.
 Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
-Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
-Generare numeri random al posto degli 0 nelle proprietà “punti fatti” e “falli subiti”.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.*/
+const teams = {
+    nome : "juventus",
+    puntiFatti : 0,
+    falliSubiti : 0
+}
+const teams2 = {
+    nome : "milan",
+    puntiFatti : 0,
+    falliSubiti : 0
+}
+const teams3 = {
+    nome : "inter",
+    puntiFatti : 0,
+    falliSubiti : 0 
+}
+const teams4 = {
+    nome : "napoli",        
+    puntiFatti : 0,
+    falliSubiti : 0
+}
+const teams5 = {
+    nome : "roma",
+    puntiFatti : 0,
+    falliSubiti : 0
+}
+/*Generare numeri random al posto degli 0 nelle proprietà “punti fatti” e “falli subiti”.
 Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.*/
+const arrayteames = [teams, teams2, teams3, teams4, teams5];
+
+
+ for (let i = 0; i < arrayteames.length; i++) {
+    arrayteames[i].puntiFatti = Math.floor(Math.random() * 100);
+    arrayteames[i].falliSubiti = Math.floor (Math.random() * 100);  
+    }
+
